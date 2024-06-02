2230d8bf225abb8297f8376d50cfaa4d4aa5539f
Jerdna <@>
2024-06-02 03:03:52 +0200

    generate image

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const { ethers } = require('ethers');
 const cors = require('cors');
 const mysql = require('mysql2');
 const { exec } = require('child_process');
-
+const path = require('path');
 
 require('dotenv').config();
 const app = express();
 
 const verifyToken = (req, res, next) => {
   const token = req.headers['authorization'];
-
+  console.log(token);
   if (!token) {
     return res.status(403).json({ success: false, message: 'No token provided' });
   }
@@ -22,7 +22,7 @@ const verifyToken = (req, res, next) => {
       return res.status(500).json({ success: false, message: 'Failed to authenticate token' });
     }
     
-    req.userAddress = decoded.address;
+    req.body.userAddress = decoded.address;
     next();
   });
 };
@@ -51,15 +51,14 @@ connection.connect((err) => {
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'http://localhost:5174/',
+  origin: '*',
   credentials: true
 }));
 
 const nonces = {};
 
 const createSmartContract = (ipfsUrl, title, amountNft, pricePerNft, donationAddr, donatationPercentage) => {
-  // TODO
-
+  
   return 'contract_adddrrrr';
 }
 
@@ -80,7 +79,7 @@ app.post('/auth', async (req, res) => {
   try {
     const signerAddress = ethers.verifyMessage(message, signature);
     if (signerAddress.toLowerCase() === address.toLowerCase()) {
-      const token = jwt.sign({ address }, process.env.jwt_secret, { expiresIn: '1h' });
+      const token = jwt.sign({ address }, process.env.jwt_secret, { expiresIn: '24h' });
       delete nonces[address];
       res.json({ token });
     } else {
@@ -97,17 +96,21 @@ app.post('/generate-image', verifyToken, async (req, res) => {
     name, 
     location
   } = req.body;
-  const pythonCommand = `./0_make_nft.py "#ff5733" "${location}" "${name}" "${description}" "sample.png"`;
-  const activateEnvCommand = 'source path/to/your/env/bin/activate';
-  const command = `${activateEnvCommand} && ${pythonCommand}`;
 
+  const eventName = name;
+  const modifiedString = eventName.replace(/ /g, '_');
+  let finalImageName = modifiedString.replace(/[^a-zA-Z0-9_]/g, '');
+  finalImageName = finalImageName + '_' + Math.round(Math.random() * 10000)
+  const pythonCommand = `./0_make_nft.py "#ff5733" "${location}" "${name}" "${description}" "sample_images/${finalImageName}.png"`;
+  const command = `${pythonCommand}`;
+  console.log(command);
   exec(command, (error, stdout, stderr) => {
     if (error) {
         console.error(`Error executing command: ${error}`);
+        res.status(500).send('Server error');
         return;
     }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
+    res.status(200).send(`${finalImageName}.png`);
   });
 });
 
@@ -115,6 +118,7 @@ app.post('/generate-image', verifyToken, async (req, res) => {
 app.post('/create-event', verifyToken, async (req, res) => {
   const {
     description, 
+    contractAddr,
     name, 
     startDate, 
     endDate, 
@@ -127,8 +131,6 @@ app.post('/create-event', verifyToken, async (req, res) => {
     userAddress
   } = req.body;
 
-  const contractAddr = createSmartContract('ipfsUrl', 'title', 'amountNft', 'pricePerNft', 'donationAddr', 'donatationPercentage');
-  
   const query = `
     INSERT INTO events (
       owner_addr, contract_addr, description, name, start_date, end_date, location, capacity, price, donation_addr, donation_percentage, image
@@ -149,8 +151,24 @@ app.post('/create-event', verifyToken, async (req, res) => {
     donatationPercentage,
     image
   ];
+  const command =  `./0_upload_ipfs.py "${location}" "${name}" "${description}" "./sample_images/${image}"`;
 
-  
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Error executing command: ${error}`);
+        res.status(500).send('Server error');
+        return;
+    }
+    console.log(stdout);
+
+    connection.query(query, values, (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ success: false, message: 'Database query failed' });
+      }
+      res.status(200).send(stdout);
+    });
+  });
     // ./0_upload_ipfs.py "Global, Worldwide" "The Great Meme Contest: " "We cordially invite you to part!" "./sample_images/output_nft_image.png"
     // `NAME="${name}" truffle compile' 
     // truffle console --network bscTestnet
@@ -162,15 +180,6 @@ app.post('/create-event', verifyToken, async (req, res) => {
     //let collectionId = await instance.createNFTCollection( "ipfs://QmPPWbX4H9xUcJWnNzKpV1Br5F4DLuv1bDmTQNZy2X5bUE/", 5, web3.utils.toWei('0.01', 'ether'), "0x142f9DE19A405a1E8B6b71811414110b33998b88", 50);
 
     //await instance.mintNFT(collectionId.logs[0].args.tokenId, { from: accounts[0], value: web3.utils.toWei('0.01', 'ether') });
-
-  
-  connection.query(query, values, (err, results) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ success: false, message: 'Database query failed' });
-    }
-    res.status(200).json({ success: true, message: 'Event created successfully' });
-  });
 });
 
 app.get('/events', (req, res) => {
@@ -184,17 +193,44 @@ app.get('/events', (req, res) => {
   });
 });
 
-app.post('/join-event', verifyToken, (req, res) => {
-  const {
-    userAddress, 
-    eventId
-  } = req.body;
-  // TODO: write to table user_events user_addr ;
+app.get('/my-events', verifyToken, (req, res) => {
+  console.log('body', req.body);
+  const query = `SELECT * FROM events WHERE owner_addr="${req.body.userAddress}"`;
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ success: false, message: 'Database query failed' });
+    }
+    res.status(200).json({ success: true, events: results });
+  });
 });
 
+app.post('/join-event', verifyToken, (req, res) => {
+  const {
+    eventId,
+    userAddress
+  } = req.body;
 
+  const query = `
+    INSERT INTO user_events (
+      user_addr, event_id
+    ) VALUES (?, ?)
+  `;
+
+  const values = [userAddress, eventId];
+
+  connection.query(query, values, (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ success: false, message: 'Database query failed' });
+    }
+    res.status(200).json({ success: true, message: 'User joined event successfully' });
+  });
+});
 
 const PORT = 3000;
+app.use('/images', express.static(path.join(__dirname, 'sample_images')));
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
